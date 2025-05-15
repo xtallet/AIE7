11b58a73748a85c65e87130691558fd495fdbf2f
Katerina Gawthorpe <xzimk04@gmail.com>
2025-05-15 10:40:35 -0400

    I added hello_world.py

--- On_ramp/PythonicRAG/chainlit.md
+++
@@ -1,3 +0,0 @@
-# Welcome to Chat with Your Text File
-
-With this application, you can chat with an uploaded text file that is smaller than 2MB!
---
+++ On_ramp/main.py
@@ -0,0 +1,20 @@
+# hello.py
+print("Hello, World!")
+
+#### ❓Question #1:
+# What is your name?
+print("What is your name?")
+name = # Add your name here
+print("Hello, " + name + "!")
+
+#### ❓Question #2:
+# Where do you live?
+print("Where do you live?")
+location = # Add your location here
+print("I live in " + location + ".")
+
+
+
+
+
+
