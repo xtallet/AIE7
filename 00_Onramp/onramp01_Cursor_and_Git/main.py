8421222a576fd3e7eac1ec39b48c7d1348249724
xtallet <xavi.tallet@gmail.com>
2025-05-25 00:51:28 +0200

    onramp01 main file updated

--- 00_Onramp/onramp01_Cursor_and_Git/main.py
+++ 00_Onramp/onramp01_Cursor_and_Git/main.py
@@ -4,17 +4,11 @@ print("Hello, World!")
 #### ❓Question #1:
 # What is your name?
 print("What is your name?")
-name = # Add your name here
+name = "Xavi"
 print("Hello, " + name + "!")
 
 #### ❓Question #2:
 # Where do you live?
 print("Where do you live?")
-location = # Add your location here
+location = "Sant Climent de Llobregat"
 print("I live in " + location + ".")
-
-
-
-
-
-
